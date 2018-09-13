712b1eb219a9685d0eaeffbb77a9af7e12a9e2b3
PrinceNoctis <ssaad363@gmail.com>
2018-09-13 13:43:20 -0600

    d

--- assssw/p5/sketch.js
+++ assssw/p5/sketch.js
@@ -1,14 +1,31 @@
-// Project Title
-// Your Name
-// Date
+// Drawing Ellipses and Rects
+// Dan Schellenberg
+// Sept 12
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - not expert yet....
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+  background(255);
 }
 
 function draw() {
+  if (mouseIsPressed && keyIsPressed) {
+    if (key === "r") {
+      rect(mouseX, mouseY, 10, 10);
+    }
+  	if (key === "e") {
+      ellipse(mouseX, mouseY, 10, 10);
+    }
+  }
+}
 
+function keyTyped() {
+  if (key === "w") {
+    background(255);
+  }
+  else if (key === "b") {
+    background(0);
+  }
 }
