a1379296c719eb1c101c84fee68c7d95b45cef45
princeNoctis <43047382+princeNoctis@users.noreply.github.com>
2018-09-18 17:35:53 -0600

    Project

--- Project/p5/sketch.js
+++ Project/p5/sketch.js
@@ -8,14 +8,15 @@ var stars = [];
 var speed;
 
 function setup() {
-  createCanvas(600, 600);
-  for (var i = 0; i < 800; i++) {
+  createCanvas(windowWidth, windowHeight);
+  for (var i = 0; i < 400; i++) {
     stars[i] = new Star();
   }
 }
 
 function draw() {
-  speed = map(mouseX, 0, width, 0, 50);
+  text('BETTER IN FULLSCREEN',width/2 , height///2)
+  speed = map(mouseX, 3, width, 0, 50);
   background(0);
   translate(width / 2, height / 2);
   for (var i = 0; i < stars.length; i++) {
