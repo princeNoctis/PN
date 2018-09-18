696332232755c487d51888d7328f967618a56443
PrinceNoctis <ssaad363@gmail.com>
2018-09-18 13:51:19 -0600

    sd

--- assssw/p5/sad/p5/sketch.js
+++ assssw/p5/sad/p5/sketch.js
@@ -8,6 +8,12 @@ let dvd;
 let x, y;
 let dx, dy;
 
+var xspeed = 2.8; // Speed of the shape
+var yspeed = 2.2; // Speed of the shape
+
+var xdirection = 1; // Left or Right
+var ydirection = 1; // Top to Bottom
+
 function preload() {
   dvd = loadImage("assets/DVD.png");
 }
@@ -33,4 +39,17 @@ function moveDVD() {
 function displayDVD() {
   background(80, 80, 80);
   image(dvd, x, y);
+}
+
+function draw() {
+  xpos = xpos + xspeed * xdirection;
+  ypos = ypos + yspeed * ydirection;
+
+  if (xpos > width - rad || xpos < rad) {
+    xdirection *= -1;
+  }
+  if (ypos > height - rad || ypos < rad) {
+    ydirection *= -1;
+  }
+
 }
