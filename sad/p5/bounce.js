6a8924350686ab5ea00361185eac9a997f7f174f
PrinceNoctis <ssaad363@gmail.com>
2018-09-18 13:38:06 -0600

    f

--- sad/p5/bounce.js
+++ sad/p5/bounce.js
@@ -14,13 +14,23 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = width/2 - dvd.height;
-  y = height/2 - ;
+  x = width/2 - dvd.width / 2;
+  y = height/2 - dvd.height /2;
   dx = random(3, 8);
   dy = random(3, 8);
 }
 
 function draw() {
+  moveDVD();
+  displayDVD();
+}
+
+function moveDVD() {
+  x += dx;
+  y += dy;
+}
+
+function displayDVD() {
   background(80, 80, 80);
-  image(dvd, 0, 0);
+  image(dvd, x, y);
 }
