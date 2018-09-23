0c5d805d51ff833dcfa790964072e4e14945861c
princeNoctis <43047382+princeNoctis@users.noreply.github.com>
2018-09-23 08:49:14 -0600

    d

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,16 +1,24 @@
 let timer = 5
+var button;
+
 
 function setup() {
   createCanvas(800, 600);
+  img = loadImage("assets/gardener.png");  // loading the image of the gardener
+  button = createButton('submit');
+
+
 }
 
 function draw() {
   background(220);
   textSize(25);
+  // loading the image of the gardener
+  image(img, 0,50, img.width/2, img.height/2);
   text(timer, width/2,50);
 
   // frameCount --> this keeps track of the number of times the program has gone throught the code, 60 = 1 second
-  // % ---> this is the Modulo operator, it divides numbers and evaluates to the remainder: 17 % 5 = 2 
+  // % ---> this is the Modulo operator, it divides numbers and evaluates to the remainder: 17 % 5 = 2
   // this can be used to determine if the number on the left is divisible by the number on the right
 
   if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
