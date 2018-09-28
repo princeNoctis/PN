67fbd723fbf899256a1f211ab4f189aeacb2de78
princeNoctis <43047382+princeNoctis@users.noreply.github.com>
2018-09-27 21:02:45 -0600

    d

--- p5-2/sketch.js
+++ p5-2/sketch.js
@@ -1,12 +1,43 @@
 var button;
-function setup(){
-	createCanvas(400, 200);
-	background(0);
-	let button = createButton("Simple Button");
-	button.position(10, 300);
-	button.mousePressed(clickFunction);
+var x = 200;
+var y = 200;
+
+
+function setup() {
+	let button = createButton("reset");
+	button.position(700, 650);
+	button.mousePressed(resetSketch);
+  createCanvas(600, 600);
 }
 
+function draw() {
+  background(220);
+  fill(0);
+  ellipse(x,y,50,50);
+	rect(width/2,height/2,50,100);
+
+  //x = x + 1;
+
+
+  if (x >= 600){
+   x = 0;
+  }
+
+}
 function clickFunction(){
 	background(random(255));
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    y = y - 20;
+  } else if (keyCode === DOWN_ARROW) {
+   y = y + 20;
+  }
+  if (keyCode === LEFT_ARROW) {
+    x = x - 20;
+  } else if (keyCode === RIGHT_ARROW) {
+    x = x + 20;
+  }
+
 }
