90d40277095b2f84c4913165c7833f65c7f86baf
PrinceNoctis <ssaad363@gmail.com>
2018-10-01 13:13:54 -0600

    e

--- trafficlight/sketch.js
+++ trafficlight/sketch.js
@@ -6,35 +6,93 @@
 // changing according to time. You may want to investigate the millis()
 // function at https://p5js.org/reference/
 
-millis();
+let state;
+let lastTimeSwitchedColor;
+
+const red = 3000;
+const green = 4000;
+const yellow = 1000;
 
 function setup() {
   createCanvas(600, 600);
+  state = 1;
+  lastTimeSwitchedColor = 0;
 }
 
 function draw() {
-  background(255);
-  drawOutlineOfLights();
-}
-
-function drawOutlineOfLights() {
-  //box
   rectMode(CENTER);
   fill(0);
   rect(width/2, height/2, 75, 200, 10);
-
-  //lights
-  if (millis() >= 4000) {
+  background(255);
+  drawOutlineOfLights();
+  diplayCorrectLight();
+  checkForStateChange();
+}
+function drawOutlineOfLights() {
+  function diplayRedLight() {
     fill(255,0,0);
     ellipse(width/2, height/2 - 65, 50, 50); //top
-    fill(255);
   }
-  else{
-    fill(255);
-    ellipse(width/2, height/2 - 65, 50, 50); //top
+
+  function diplayYellowLight() {
+    fill(255,255,0);
+    ellipse(width/2, height/2, 50, 50); //middle
   }
-  fill(255);
+
+  function diplayGreenLight() {
+    fill(0,255,0);
+    ellipse(width/2, height/2 + 65, 50, 50); //bottom
+  }
+}
+function diplayRedLight() {
+  fill(255,0,0);
+  ellipse(width/2, height/2 - 65, 50, 50); //top
+}
+
+function diplayYellowLight() {
+  fill(255,255,0);
   ellipse(width/2, height/2, 50, 50); //middle
-  fill(255);
+}
+
+function diplayGreenLight() {
+  fill(0,255,0);
   ellipse(width/2, height/2 + 65, 50, 50); //bottom
 }
+
+function diplayCorrectLight() {
+  if (state === 1) {
+    diplayRedLight();
+  }
+  else if (state === 2) {
+    diplayYellowLight();
+  }
+  else if (state === 3) {
+    diplayGreenLight();
+  }
+}
+function checkForStateChange() {
+  let et = millis() - lastTimeSwitchedColor;
+  if (state === 1 && et >= red){
+    state = 2;
+    lastTimeSwitchedColor = millis();
+  }
+  else if (state === 2 && et >= yellow){
+    state = 3;
+    lastTimeSwitchedColor = millis();
+  }
+  else if (state === 3 && et >= green){
+    state = 1;
+    lastTimeSwitchedColor = millis();
+  }
+}
+
+//box
+rectMode(CENTER);
+fill(0);
+rect(width/2, height/2, 75, 200, 10);
+
+//lights
+fill(255);
+ellipse(width/2, height/2 - 65, 50, 50); //top
+ellipse(width/2, height/2, 50, 50); //middle
+ellipse(width/2, height/2 + 65, 50, 50); //bottom
