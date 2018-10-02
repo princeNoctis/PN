a215ff690abb4eb944438cd9db59466e9f1d4179
princeNoctis <43047382+princeNoctis@users.noreply.github.com>
2018-10-02 17:44:14 -0600

    d

--- Project/p5/Intereactive scene.js
+++ Project/p5/Intereactive scene.js
@@ -11,13 +11,13 @@ let spellSound;
 let pat;
 
 function preload() {
-  // preload the background music and spell sound
+  // preload the background music and spell sound Bazinga!
   backgroundMusic = loadSound("assets/326553__shadydave__the-sonata-piano-loop.mp3");
   spellSound = loadSound("assets/Bazinga Sound Clip.mp3");
 }
 
 function setup() {
-  // center the square, make the background music loop and setVolume
+  // center the square, make the background music loop and setVolume and Patricks image
   rectMode(CENTER);
   createCanvas(windowWidth, windowHeight);
   backgroundMusic.setVolume(0.4);
@@ -27,20 +27,20 @@ function setup() {
 }
 
 function draw() {
-  //text size, text and mouse pressed, key pressed function
+  //text size, text and mouse pressed, key pressed functions, displaying patrick image and code for the square in the middle of screen.
   textSize(8);
   text("zoom in and out with mousewheel and hold mouse button + press e ", 22, 130);
   text("w for white screen or reset", 22, 150);
   text("b for black screen", 22, 170);
   text(" press any mouse button to make Sheldon say Bazinga!! ", 22, 190);
   text(" and lastly press s and mouse for Patrick!! ", 22, 220);
-  //
+  // display ellipse after sheldons bazinga voice
   if (mouseIsPressed && keyIsPressed) {
     if (key === "e") {
       ellipse(mouseX, mouseY, 30, 30);
     }
   }
-  //
+  //displaying patrick near mouse cursor
   if (keyIsPressed) {
     if (key === "s") {
       image(pat, mouseX, mouseY, 100, 100);
@@ -54,17 +54,16 @@ function draw() {
  pop();
 
 }
-
+// able to resize canvas
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-
+//function for mousewheel
 function mouseWheel(event) {
-  //println(event.delta);
   pos += event.delta;
   posRev = height - 10 - pos;
 }
-
+// functions for black and white screen
 function keyTyped() {
   if (key === "w") {
     background(255);
