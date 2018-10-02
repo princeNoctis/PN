009c29d2623a7019c72a7dca9287871bf387e5d7
princeNoctis <43047382+princeNoctis@users.noreply.github.com>
2018-10-02 17:22:23 -0600

    s

--- Project/p5/Intereactive scene.js
+++ Project/p5/Intereactive scene.js
@@ -2,34 +2,57 @@
 // Muhammad Sheikh
 // Mon.24,2018
 // Extra for Experts:
-// - added music, keyIsPressed
+// - added music, keyIsPressed mouse zoom out, and made own shape and resize window
 
+// varaiables and Lets
+let pos = 25;
 let backgroundMusic;
 let spellSound;
 
-
 function preload() {
+  // preload the background music and spell sound
   backgroundMusic = loadSound("assets/326553__shadydave__the-sonata-piano-loop.mp3");
-  spellSound = loadSound("assets/Hitmarker - MLG Sound Effect (HD).mp3");
+  spellSound = loadSound("assets/Bazinga Sound Clip.mp3");
 }
 
 function setup() {
-  createCanvas(400, 400);
-  background(255);
-  backgroundMusic.setVolume(0.2);
+  // center the square, make the background music loop and setVolume
+  rectMode(CENTER);
+  createCanvas(windowWidth, windowHeight);
+  backgroundMusic.setVolume(0.4);
+  spellSound.setVolume(0.9);
   backgroundMusic.loop();
 }
 
 function draw() {
+  //text size, text and mouse pressed, key pressed function
+  textSize(8);
+  text("zoom in and out with mousewheel and hold mouse button + press e ", 22, 130);
+  text("w for white screen or reset", 22, 150);
+  text("b for black screen", 22, 170);
+  text(" press any mouse button to make Sheldon say Bazinga!! ", 22, 150);
   if (mouseIsPressed && keyIsPressed) {
-    if (key === "r") {
-      random(255,255,255);
-      rect(mouseX, mouseY, 10, 10,random(255,255,255));
-    }
-  	if (key === "e") {
-      ellipse(mouseX, mouseY, 10, 10,random(255,255,255));
+    if (key === "e") {
+      ellipse(mouseX, mouseY, 30, 30);
     }
   }
+  // the square is rgb and turn bigger and smaller when scrolled down and up
+ push();
+ noStroke();
+ fill(random(255),random(255),random(255));
+ rect(width / 2, height / 2, 100 + pos, 100 + pos);
+ pop();
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseWheel(event) {
+  //println(event.delta);
+  pos += event.delta;
+  posRev = height - 10 - pos;
 }
 
 function keyTyped() {
@@ -42,5 +65,7 @@ function keyTyped() {
 }
 
 function mousePressed() {
+  // make the text turn random colors when clicked
+  fill(random(255),random(255),random(255));
   spellSound.play();
 }
