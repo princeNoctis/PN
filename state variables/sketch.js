3f046cc6ba93fd4a4dc2966f2f3bb3f971060f5d
princeNoctis <43047382+princeNoctis@users.noreply.github.com>
2018-10-12 17:59:26 -0600

    Update state variables/sketch.js

--- state variables/sketch.js
+++ state variables/sketch.js
@@ -24,23 +24,21 @@ function setup() {
 
 function draw() {
   background(0);
-<<<<<<< HEAD
+
   overCircle = dist(mouseX,mouseY,x,y)
   if overCircle > RADIUS {
     fill(134,155,243);
-=======
+
   stroke(50);
   if (mouseX > box-circleSize && mouseX < box+circleSize &&
       mouseY > boy-circleSize && mouseY < boy+circleSize) {
     overcircle = true;
->>>>>>> 3714f74fb883e4ff1bb971bc2080ee05c522fc51
   }
   else {
     fill(244,56,200);
   }
-<<<<<<< HEAD
   ellipse(RADIUS)
-=======
+
   ellipse(box, boy, circleSize, circleSize);
   translate(box,boy);
   box = box + random(-3, 2);
@@ -52,7 +50,6 @@ function draw() {
   if (box < 0) {
     box = windowWidth;
   }
->>>>>>> 3714f74fb883e4ff1bb971bc2080ee05c522fc51
 
 }
 
