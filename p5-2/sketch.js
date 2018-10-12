81e72cc6d7018218a67e6ad45e29c54f8d7a74a2
PrinceNoctis <ssaad363@gmail.com>
2018-10-12 13:42:18 -0600

    s

--- p5-2/sketch.js
+++ p5-2/sketch.js
@@ -1,144 +1,40 @@
 // State Variables
-// Tyler Boechler
+//
 // September 28th, 2018
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let snakeSpeed;
-
-let middleX;
-let middleY;
-
-let movementTimer = 250;
-
-let xValuesList;
-let yValuesList;
-
-let directionState;
-let gameState;
-
-let initialTime;
-let snakeSize;
-let numberOfCubes;
+let ballArray = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  middleX = width/2;
-  middleY = height/2;
+  createCanvas(400, 400);
 
-  xValuesList = [middleX];
-  yValuesList = [middleY];
-
-  directionState = 1;
-  snakeSize = 33;
-  snakeSpeed = 35;
-  numberOfCubes = 1;
-
-  initialTime = 0;
-
-  gameState = 2;
 }
 
 function draw() {
-  if (gameState === 1) {
-    menu();
-  }
-  else if (gameState === 2) {
-    background(0);
-    // drawFood();
-    moveSnake();
-    drawSnakeCubes();
-
-    // hitSnake();
-    // touchingFood();
-  }
-  else if (gameState === 3) {
-    //pause stuffs
-  }
-  else {
-    //Game over stuffs
+  background(220);
+  for (let i=0; i<ballArray.length; i++) {
+    fill(ballArray[i].fillColor);
+    ellipse(ballArray[i].x, ballArray[i].y, ballArray[i].radius*2, ballArray[i].radius*2);
   }
 }
 
-function menu() {
-
+function mousePressed() {
+  let ball = {
+    x: mouseX,
+    y: mouseY,
+    radius: 25,
+    fillColor: color(255, 0, 0),
+  };
+  ballArray.push(ball);
 }
 
-function moveSnake() {
-  let elapsedTime = millis() - initialTime;
-
-  if (directionState === 1 && elapsedTime >= movementTimer) {
-    xValuesList = append(xValuesList, xValuesList[0] - snakeSpeed);
-    xValuesList = shorten(xValuesList);
+function keyTyped() {
+  if (key === "w") {
+    ball.radius *= 1.1;
   }
-
-  else if (directionState === 2 && elapsedTime >= movementTimer) {
-    yValuesList = append(yValuesList, yValuesList[0] - snakeSpeed);
-    yValuesList = shorten(yValuesList);
-  }
-
-  else if (directionState === 3 && elapsedTime >= movementTimer) {
-    xValuesList = append(xValuesList, xValuesList[0] + snakeSpeed);
-    xValuesList = shorten(xValuesList);
-  }
-
-  else if (directionState === 4 && elapsedTime >= movementTimer) {
-    yValuesList = append(yValuesList, yValuesList[0] + snakeSpeed);
-    yValuesList = shorten(yValuesList);
-  }
-
-  initialTime = millis();
-}
-
-function drawSnakeCubes() {
-  fill(0, 255, 0);
-  for (let listSpot = 0; listSpot < numberOfCubes; listSpot ++) {
-    rect(xValuesList[listSpot], yValuesList[listSpot], snakeSize, snakeSize);
-  }
-}
-
-function drawFood() {
-
-}
-
-function touchingFood() {
-
-}
-
-function hitSnake() {
-
-}
-
-function addSnake() {
-
-}
-
-
-function keyPressed() {
-  //LEFT
-  if (key === "a" || key === "A") {
-    directionState = 1;
-  }
-  //UP
-  else if (keyCode === 38) {
-    directionState = 2;
-  }
-  //RIGHT
-  else if (keyCode === 39) {
-    directionState = 3;
-  }
-  //DOWN
-  else if (keyCode === 40) {
-    directionState = 4;
-  }
-  //Pausing
-  if (key === "p" || key === "P") {
-    if (gameState === 2) {
-      gameState = 3;
-    }
-    else if (gameState === 3) {
-      gameState = 2;
-    }
+  else if (key === "s") {
+    ball.radius *= 0.9;
   }
 }
--- state variables/sketch.js
+++ state variables/sketch.js
@@ -13,35 +13,49 @@ let xOffset = 0.0;
 let yOffset = 0.0;
 let x, y;
 let state;
-
+let ballArray = [];
 
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  state = 1;
   box = windowWidth;
   boy = windowHeight;
   ellipseMode(RADIUS);
   noStroke();
+  for (let box =0; box<ballArray.length; box++) {
+    for (let boy =0; boy<ballArray.length; boy++) {
+      fill(ballArray[box].fillColor);
+      ellipse(ballArray[box].x, ballArray[box].y, ballArray[box].radius*2, ballArray[box].radius*2);
+    }
+  }
 }
 
 function draw() {
-  background(0);
+  if (state === 1) {
+    displayStartScreen();
+  }
+  if (state === 2) {
+    moveThing();
+    displayThing();
+  }
 
+  background(0);
+  if ( boy === windowHeight) {
+
+  }
   stroke(50);
   if (mouseX > box-circleSize && mouseX < box+circleSize &&
       mouseY > boy-circleSize && mouseY < boy+circleSize) {
     overcircle = true;
-
-
   }
   else {
     fill(244,56,200);
   }
 
   ellipse(RADIUS);
-  ellipse(box, boy, circleSize, circleSize);
   translate(box,boy);
   box = box + random(-8, 2);
   boy = boy - 4;
@@ -56,10 +70,32 @@ function draw() {
 
 }
 
+function score() {
+
+}
+
 function mousePressed() {
   if(overcircle) {
     fill(random(255, 255, 255));
   }
   xOffset = mouseX-box;
   yOffset = mouseY-boy;
+}
+function displayStartScreen() {
+  let buttonWidth = 400;
+  let buttonHeight = 200;
+  let leftSide = width / 2 - buttonWidth / 2;
+  let topSide = height / 2 - buttonHeight / 2;
+  let rightSide = leftSide + buttonWidth;
+  let bottomSide = topSide + buttonHeight;
+
+  fill(0);
+  if (mouseX >= leftSide && mouseX <= rightSide && mouseY >= topSide && mouseY <= bottomSide) {
+    fill(125);
+    if (mouseIsPressed) {
+      state = 2;
+    }
+  }
+
+  rect(leftSide, topSide, buttonWidth, buttonHeight);
 }
