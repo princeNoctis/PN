5d374db38e8c0a94792506e7958b2fa7d520b9ac
princeNoctis <43047382+princeNoctis@users.noreply.github.com>
2018-10-15 17:06:18 -0600

    Update p5-1/sketch.js

--- p5-1/sketch.js
+++ p5-1/sketch.js
@@ -13,17 +13,76 @@ let overBoc = false;
 let locked = false;
 let xOffset = 0.0;
 let yOffset = 0.0;
+let time = 5;
+let state;
+
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(windowWidth, windowHeight);
   cx = width/2.0;
   cy = height/2.0;
   ellipseMode(RADIUS);
   strokeWeight(2);
+  state = 1;
 }
 
 function draw() {
-  background(237,34,93);
+  if (state === 1) {
+    displayStartScreen();
+  }
+  else if (state === 2) {
+    displaygame();
+  }
+}
+
+function displayStartScreen() {
+  let buttonWidth = 400;
+  let buttonHeight = 200;
+  let leftSide = width / 2 - buttonWidth / 2;
+  let topSide = height / 2 - buttonHeight / 2;
+  let rightSide = leftSide + buttonWidth;
+  let bottomSide = topSide + buttonHeight;
+
+  fill(0);
+  if (mouseX >= leftSide && mouseX <= rightSide && mouseY >= topSide && mouseY <= bottomSide) {
+    fill(125);
+    if (mouseIsPressed) {
+      state = 2;
+    }
+  }
+
+  rect(leftSide, topSide, buttonWidth, buttonHeight);
+}
+
+function mousePressed() {
+
+
+  if(overBoc) {
+    locked = true;
+    fill(255, 255, 255);
+  } else {
+    locked = false;
+  }
+  xOffset = mouseX-bx;
+  yOffset = mouseY-by;
+
+}
+
+function timer() {
+  textAlign(CENTER, CENTER);
+  textSize(25);
+  text(time, width/2, height/2);
+  if (frameCount % 60 === 0 && time > 0) {
+    time --;
+  }
+  if (time === 0) {
+    text("!!GAME OVER!!", width/2, height*0.7);
+    noLoop();
+  }
+}
+
+function displaygame() {
+  background(234,12,123);
 
   // Test if the cursor is over the box
   if (mouseX > cx-bocSize && mouseX < cx+bocSize &&
@@ -38,20 +97,18 @@ function draw() {
     fill(244,122,158);
     overBoc = false;
   }
+  timer();
+  // Jiggling randomly on the horizontal axis
+  cx = cx + random(-1, 1);
+  // Moving up at a constant speed
+  cy = cy - 1;
 
+  // Reset to the bottom
+  if (cy < 0) {
+    cy = height;
+  }
   // Draw the box
   ellipse(cx, cy, bocSize, bocSize);
-}
-
-function mousePressed() {
-  if(overBoc) {
-    locked = true;
-    fill(255, 255, 255);
-  } else {
-    locked = false;
-  }
-  xOffset = mouseX-bx;
-  yOffset = mouseY-by;
 
 }
 
