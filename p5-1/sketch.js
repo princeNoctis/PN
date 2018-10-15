f20c4494e0daf4d617469b5ed4af02ac6f438004
princeNoctis <43047382+princeNoctis@users.noreply.github.com>
2018-10-15 15:46:34 -0600

    s

--- p5-1/sketch.js
+++ p5-1/sketch.js
@@ -4,56 +4,7 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-let cx;
-let cy;
-let circleSize = 75;
-let overcircle = false;
-let locked = false;
-let xOffset = 0.0;
-let yOffset = 0.0;
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  cx = width/2.0;
-  cy = height/2.0;
-  ellipseMode(RADIUS);
-  strokeWeight(2);
-}
-
-function draw() {
-  background(0,0,200);
-
-  // Test if the cursor is over the box
-  if (mouseX > cx-circleSize && mouseX < cx+circleSize &&
-      mouseY > cy-circleSize && mouseY < cy+circleSize) {
-    overcircle = true;
-    if(!locked) {
-      stroke(255);
-      fill(244,122,158);
-    }
-  } else {
-    stroke(156,39,176);
-    fill(244,122,158);
-    overcircle = false;
-  }
-
-  // Draw the box
-  ellipse(cx, cy, circleSize, circleSize);
-}
-
-function mousePressed() {
-  if(overcircle) {
-    locked = true;
-    fill(255, 255, 255);
-  } else {
-    locked = false;
-  }
-  xOffset = mouseX-cx;
-  yOffset = mouseY-cy;
-
-}
-
-function mouseReleased() {
-  locked = false;
+// global variables
+function Game() {
+  
 }
--- state variables/sketch.js
+++ state variables/sketch.js
@@ -6,139 +6,140 @@
 // - describe what you did to take this project "above and beyond"
 // got some help from p5js website.
 
+function game() {
+  let circleSize = 30;
+  let clickedCircle = false;
 
-let circleSize = 30;
-let clickedCircle = false;
-
-let box;
-let boy;
-let x, y;
-let state;
-
-
-let startScreen;
-let timer = 10;
-
-let score;
+  let box;
+  let boy;
+  let x, y;
+  let state;
 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  box = windowWidth;
-  boy = windowHeight;
-  ellipseMode(RADIUS);
-  state = 1;
-  startScreen = loadImage("assets/start screen.PNG");
-  score = createDiv("Score = 0");
-  score.position(20, 20);
-  score.id = "score";
-  score.style("color", "white");
-}
+  let startScreen;
+  let timer = 10;
 
-function draw() {
+  let score;
 
-  background(0);
-  stroke(220);
-  if (mouseX > box-circleSize && mouseX < box+circleSize &&
-      mouseY > boy-circleSize && mouseY < boy+circleSize) {
-    clickedCircle = true;
+
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+    box = windowWidth;
+    boy = windowHeight;
+    ellipseMode(RADIUS);
+    state = 1;
+    startScreen = loadImage("assets/start screen.PNG");
+    score = createDiv("Score = 0");
+    score.position(20, 20);
+    score.id = "score";
+    score.style("color", "white");
   }
-  time();
-  texts();
-  if (state === 1) {
-    displayStartScreen();
-  }
-  else if (state === 2) {
-    ellipse(box, boy, circleSize, circleSize);
-    box = box + random(-8, 2);
-    boy = boy - 4;
-    if (boy < 0) {
-      boy = windowHeight;
-      fill(255);
+
+  function draw() {
+
+    background(0);
+    stroke(220);
+    if (mouseX > box-circleSize && mouseX < box+circleSize &&
+        mouseY > boy-circleSize && mouseY < boy+circleSize) {
+      clickedCircle = true;
     }
-    mouseIsPressed();
-    if (box < 0) {
-      box = windowWidth;
-      fill(255);
+    time();
+    texts();
+    if (state === 1) {
+      displayStartScreen();
+    }
+    else if (state === 2) {
+      ellipse(box, boy, circleSize, circleSize);
       box = box + random(-8, 2);
       boy = boy - 4;
-
       if (boy < 0) {
         boy = windowHeight;
         fill(255);
       }
+      mouseIsPressed();
       if (box < 0) {
         box = windowWidth;
         fill(255);
+        box = box + random(-8, 2);
+        boy = boy - 4;
+
+        if (boy < 0) {
+          boy = windowHeight;
+          fill(255);
+        }
+        if (box < 0) {
+          box = windowWidth;
+          fill(255);
+      }
+    }
+
+  }
+
+
+
+  function mousePressed() {
+    // Check if mouse is inside the circle
+    let d = dist(mouseX, mouseY, box, boy);
+    if (d < box) {
+      // Pick new random color values
+      r = random(255);
+      g = random(255);
+      b = random(255);
+      }
+    }
+
+  function displayStartScreen() {
+    let buttonWidth = windowWidth;
+    let buttonHeight = windowHeight;
+    let leftSide = width / 2 - buttonWidth / 2;
+    let topSide = height / 2 - buttonHeight / 2;
+    let rightSide = leftSide + buttonWidth;
+    let bottomSide = topSide + buttonHeight;
+    image(startScreen,0, 0);
+
+    if (mouseX >= leftSide && mouseX <= rightSide && mouseY >= topSide && mouseY <= bottomSide) {
+      fill(200,200,34);
+      if (mouseIsPressed) {
+        state = 2;
+      }
+    }
+    else {
+      fill(0);
+    }
+
+  }
+
+  function texts() {
+    fill(0,0,255);
+    text("click on circles to score points before time run out",800 ,20);
+    textSize(20);
+  }
+
+  function time() {
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    text(timer, 0, 0);
+    if (frameCount % 60 === 0 && timer > 0) {
+      timer --;
+    }
+    if (timer === 0) {
+      text("GAME OVER", width/2, height*0.7);
+    }
+
+    }
+
+  }
+
+  function scoreApoint() {
+    if (mouseClicked) {
+      let prevScore = parseInt(score.html().substring(8));
+      text("Score = " + score,"30px Comic Sans MS", 10, 30, "white");
     }
   }
 
-}
-
-
-
-function mousePressed() {
-  // Check if mouse is inside the circle
-  let d = dist(mouseX, mouseY, box, boy);
-  if (d < box) {
-    // Pick new random color values
-    r = random(255);
-    g = random(255);
-    b = random(255);
-    }
+  function switchScreen() {
+    if (state === 2) {
+      draw();
+   	}
   }
-
-function displayStartScreen() {
-  let buttonWidth = windowWidth;
-  let buttonHeight = windowHeight;
-  let leftSide = width / 2 - buttonWidth / 2;
-  let topSide = height / 2 - buttonHeight / 2;
-  let rightSide = leftSide + buttonWidth;
-  let bottomSide = topSide + buttonHeight;
-  image(startScreen,0, 0);
-
-  if (mouseX >= leftSide && mouseX <= rightSide && mouseY >= topSide && mouseY <= bottomSide) {
-    fill(200,200,34);
-    if (mouseIsPressed) {
-      state = 2;
-    }
-  }
-  else {
-    fill(0);
-  }
-
-}
-
-function texts() {
-  fill(0,0,255);
-  text("click on circles to score points before time run out",800 ,20);
-  textSize(20);
-}
-
-function time() {
-  textAlign(CENTER, CENTER);
-  textSize(20);
-  text(timer, 0, 0);
-  if (frameCount % 60 === 0 && timer > 0) {
-    timer --;
-  }
-  if (timer === 0) {
-    text("GAME OVER", width/2, height*0.7);
-  }
-
-  }
-
-}
-
-function scoreApoint() {
-  if (mouseClicked) {
-    let prevScore = parseInt(score.html().substring(8));
-    text("Score = " + score,"30px Comic Sans MS", 10, 30, "white");
-  }
-}
-
-function switchScreen() {
-  if (state === 2) {
-    draw();
- 	}
 }
