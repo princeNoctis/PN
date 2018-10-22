408ed6eabba71763325c39195efbf89a8a842e82
PrinceNoctis <ssaad363@gmail.com>
2018-10-22 14:12:26 -0600

    ball

---
+++ p5-3/sketch.js
@@ -0,0 +1,17 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  let board = [[0,1,0,0],
+             [0,0,0,1],
+             [0,0,0,0],
+             [1,1,0,0]];
+}
