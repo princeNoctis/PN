b085e4dc230defb7aaf53a0da253cb3ca0f19e27
PrinceNoctis <ssaad363@gmail.com>
2018-10-30 14:13:14 -0600

    h

---
+++ GRID/GridAssignment.js
@@ -0,0 +1,48 @@
+// Grid Assignment
+// Muhammad Sheikh
+// oct.27,2018
+// Extra for Experts:
+//
+
+let rows = 5;
+let cols = 5;
+let grid;
+let cellSize;
+
+function preload() {
+  grid = loadStrings("assets/Levels.txt");
+}
+
+function setup() {
+  createCanvas(600, 600);
+  rows = grid[0].length;
+  cols = grid[0].length;
+  cellSize = width / cols;
+  //grid = createRandom2dArray(cols, rows);
+  cleanUpTheGrid();
+}
+
+function draw() {
+  background(255);
+  displayGrid();
+}
+
+function cleanUpTheGrid() {
+  for (let i=0; i<grid.length; i++) {
+    grid[i] = grid[i].split("");  //turns it into a 2d array
+  }
+}
+
+function displayGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === "0") {
+        fill(0);
+      }
+      else {
+        fill(255);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
