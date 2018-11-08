4556b3880a0cb990f211466210c1e12796ff303f
princeNoctis <43047382+princeNoctis@users.noreply.github.com>
2018-11-07 22:56:16 -0600

    m

--- ccc/p5/sketch.js
+++ ccc/p5/sketch.js
@@ -1,32 +1,39 @@
-// grid assignment
+// gridsize assignment
 // Your Name
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+let rows = 5;
+let cols = 5;
+let cellSize;
+
+
+function preload() {
+  gridsize = loadStrings("assets/Maze.txt");
+}
+
+
 function setup() {
   createCanvas(500, 500);
   frameRate(60);
-
+  rows = gridsize[0].length;
+  cols = gridsize[0].length;
+  cellSize = width / cols;
+  cleanUpTheGrid();
   mapSize = 20; // 20 grids by 20 grids
-  gridsize = width/mapSize; // assuming width and height are the same value
   gridposition = createVector(10, 10); // start at 0, 0 (top left)
 }
 
 function draw() {
   background(0,255,0);
   stroke(127);
-  noFill();
   rectMode(CORNER);
   time();
-
+  displayGrid();
   // --- Drawing the grid
-  for (let i = 0; i < gridsize; i++) { // rows
-    for (let j = 0; j < gridsize; j++) { // columns
-      rect(gridsize * i, gridsize * j, gridsize, gridsize);
-    }
-  }
 
 
   // --- Calculating pixelposition from gridposition
@@ -37,10 +44,10 @@ function draw() {
   noStroke();
   fill(255, 0, 0);
   rectMode(CENTER);
-  rect(pixelposition.x, pixelposition.y, gridsize/2, gridsize/2);
+  rect(pixelposition.x, pixelposition.y, 15, 15);
 }
 
-function mousePressed() {
+function mouseIsPressed() {
   // Do we want to move vertically or horizontally? Lets see how close to the player in X Y we clicked
   // and calculate the abosulate value
   if (abs(mouseX - pixelposition.x) > abs(mouseY - pixelposition.y)) { // we move in X
@@ -64,7 +71,29 @@ function mousePressed() {
 
 function time() {
   let timer = 20;
-  colo
-  textSize(100);
-  text(timer, width/2, height/2);
+  stroke(timer);
+  fill(0,timer)
+  textSize(25);
+  text(timer, 250, 50);
+}
+
+
+function cleanUpTheGrid() {
+  for (let i=0; i<gridsize.length; i++) {
+    gridsize[i] = gridsize[i].split("");  //turns it into a 2d array
+  }
+}
+
+function displayGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (gridsize[y][x] === "0") {
+        fill(0);
+      }
+      else if (gridsize[y][x] === "5") {
+        fill(255);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
 }
