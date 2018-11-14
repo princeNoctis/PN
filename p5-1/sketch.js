78cd3a71e6f5ef3400dc1a3cff40d07b6fde8659
PrinceNoctis <ssaad363@gmail.com>
2018-11-14 14:06:36 -0600

    Update p5-1/sketch.js

--- p5-1/sketch.js
+++ p5-1/sketch.js
@@ -7,21 +7,22 @@
 
 
 class Particle {
-  constructor(x,y){
+  constructor(x, y) {
     this.x = x;
     this.y = y;
     this.size = 25;
-    this.dx = random(-10,10);
-    this.dy = random(-10,10);
+    this.dx = random(-10, 10);
+    this.dy = random(-10, 10);
     this.transparency = 255;
-    this.color = color(random(255),random(255),random(255),this.transparency);
+    this.color = color(random(255), random(255), random(255), this.transparency);
   }
 
   display() {
     noStroke();
     fill(this.color);
-    ellipse(this.x,this.y,this.size,this.size);
+    ellipse(this.x, this.y, this.size, this.size);
   }
+
   update() {
     this.transparency -= 5;
     this.color.setAlpha(this.transparency);
@@ -30,7 +31,6 @@ class Particle {
   }
 }
 
-let paarticle;
 let fireworks = [];
 
 function setup() {
@@ -39,15 +39,17 @@ function setup() {
 
 function draw() {
   background(0);
-  for (let i =0; i < fireworks.length; i++){
-    fireworks[i].display();
+
+  for (let i=0; i<fireworks.length; i++) {
     fireworks[i].update();
+    fireworks[i].display();
   }
+
 }
 
-function mouseIsPressed() {
-  for (let i = 0; i < 100; i++) {
-    let newPart = new paarticle(mouseX,mouseY);
-    fireworks.push(newPart);
+function mousePressed() {
+  for (let i=0; i<100; i++) {
+    let someParticle = new Particle(mouseX, mouseY);
+    fireworks.push(someParticle);
   }
 }
