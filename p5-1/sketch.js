135e79036a61c7894c66346dffffaa3d41ddd4fd
PrinceNoctis <ssaad363@gmail.com>
2018-11-14 13:54:25 -0600

    Update p5-1/sketch.js

--- p5-1/sketch.js
+++ p5-1/sketch.js
@@ -5,10 +5,42 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+class Particle {
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.size = 35;
+    this.dx = random(-10,10);
+    this.dy = random(-10,10);
+    this.transparency = 255;
+    this.color = color(random(255),random(255),random(255),this.transparency);
+  }
+
+  display() {
+    fill(this.color);
+    ellipse(this.x,this.y,this.size,this.size);
+  }
+  update() {
+    this.transparency -= 5;
+    this.color.setAlpha(this.transparency);
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+}
+
+let particle;
+
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  particle = new Particle(width/2,height/2);
 }
 
 function draw() {
-
+  particle.update();
+  background(0);
+  particle.display();
 }
