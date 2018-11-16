ec29e4c7a96210f85c00defaf296fbf9f379b8c7
PrinceNoctis <ssaad363@gmail.com>
2018-11-16 14:09:11 -0600

    game

--- p5-1/sketch.js
+++ p5-1/sketch.js
@@ -5,51 +5,104 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let gif_loadImg, gif_createImg;
+let tiles;
+let levelBackground;
+let platform, coin, box, fly, p1, slime, empty;
+let tilesHigh, tilesWide;
+let tileWidth, tileHeight;
+let levelToLoad;
+let lines;
 
-class Particle {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.size = 25;
-    this.dx = random(-10, 10);
-    this.dy = random(-10, 10);
-    this.transparency = 255;
-    this.color = color(random(255), random(255), random(255), this.transparency);
-  }
-
-  display() {
-    noStroke();
-    fill(this.color);
-    ellipse(this.x, this.y, this.size, this.size);
-  }
-
-  update() {
-    this.transparency -= 5;
-    this.color.setAlpha(this.transparency);
-    this.x += this.dx;
-    this.y += this.dy;
-  }
+function preload() {
+  //load level data
+  levelToLoad = "assets/22.txt";
+  lines = loadStrings(levelToLoad);
+  //load background
+  levelBackground = loadImage("assets/level_background.png");
+  //load tile images
+  platform = loadImage("assets/platform.png");
+  coin = loadImage("assets/coin.png");
+  box = loadImage("assets/boxItem.png");
+  fly = createImg("assets/dragon_flying.gif");
+  p1 = loadImage("assets/p1_front.png");
+  slime = loadImage("assets/slimeWalk1.png");
+  //empty = loadImage("assets/empty.png");
 }
 
-let fireworks = [];
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  // keep this a 4:3 ratio, or it will stretch in weird ways
+  createCanvas(800, 600);
+
+  tilesHigh = lines.length;
+  tilesWide = lines[0].length;
+
+  tileWidth = width / tilesWide;
+  tileHeight = height / tilesHigh;
+
+  tiles = createEmpty2dArray(tilesWide, tilesHigh);
+
+  //put values into 2d array of characters
+  for (let y = 0; y < tilesHigh; y++) {
+    for (let x = 0; x < tilesWide; x++) {
+      let tileType = lines[y][x];
+      tiles[x][y] = tileType;
+    }
+  }
 }
 
 function draw() {
-  background(0);
+  display();
+}
 
-  for (let i=0; i<fireworks.length; i++) {
-    fireworks[i].update();
-    fireworks[i].display();
+function display() {
+  image(levelBackground, 0, 0, width, height);
+
+  for (let y = 0; y < tilesHigh; y++) {
+    for (let x = 0; x < tilesWide; x++) {
+      showTile(tiles[x][y], x, y);
+    }
   }
+}
+
+function showTile(location, x, y) {
+  if (location === "#") {
+    image(platform, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "C") {
+    image(coin, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "B") {
+    image(box, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "F") {
+    fly.position(0,0);
+  }
+  else if (location === "P") {
+    image(p1, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "S") {
+    image(slime, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  // else if ( location === "+") {
+  //   image(empty, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  // }
 
 }
 
-function mousePressed() {
-  for (let i=0; i<100; i++) {
-    let someParticle = new Particle(mouseX, mouseY);
-    fireworks.push(someParticle);
+function keyPressed(){
+  if (keyCode === "RIGHT ARROW"){
+    
   }
+}
+
+function createEmpty2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      randomGrid[x].push(0);
+    }
+  }
+  return randomGrid;
 }
--- p5/sketch.js
+++ p5/sketch.js
@@ -1,16 +1,16 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+let bird_loadimg, bird_createimg;
 
-function setup() {
-  createCanvas(windowWidth,windowHeight);
-
+function preload(){
+  bird_loadimg = loadImage ("assets/sorry.gif");
+  bird_createimg = createImg("assets/sorry.gif");
 }
 
+function setup() {
+  createCanvas(500, 500);
+  background("yellow");
+  //noLoop();
+}
 
-function draw(){
-
+function draw() {
+  bird_createimg.position(width/2,height/2); //loads GIF correctly
 }
