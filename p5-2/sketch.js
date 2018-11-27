609f2d847ede1305670537f9788fb01a84cb4f21
Santanu09 <43047324+Santanu09@users.noreply.github.com>
2018-11-27 13:43:18 -0600

    Update sketch.js

--- p5-2/sketch.js
+++ p5-2/sketch.js
@@ -159,6 +159,43 @@ function createempty2dArray(cols, rows) {
   return randomGrid;
 }
 
+function keyPressed() {
+  if (keyCode === 87) { //keycode 87 = w
+    //condition for checking if the top square is white or green/destination
+    if (tiles[py-1][px] === "+" || tiles[py-1][px] === "f") {
+      tiles[py-1][px] = "S";
+      tiles[py][px] = "+";
+      py -= 1; //changes players current y position after moving
+    }
+  }
+  if (keyCode === 65) { //keycode 65 = a
+    //condition for checking if the left square is white or green/destination
+    if (tiles[py][px-1] === "+" || tiles[py][px-1] === "f") {
+      tiles[py][px-1] = "S";
+      tiles[py][px] = "+";
+      px -= 1;  //changes players current x position after moving
+    }
+  }
+  if (keyCode === 83) { //keycode 83 = s
+    //condition for checking if the bottom square is white or green/destination
+    if (tiles[py+1][px] === "+" || tiles[py+1][px] === "f") {
+      tiles[py+1][px] = "S";
+      tiles[py][px] = "+";
+      py += 1;  //changes players current y position after moving
+    }
+  }
+  if (keyCode === 68) { //keycode 68 = d
+    //condition for checking if the right square is white or green/destination
+    if (tiles[py][px+1] === "+" || tiles[py][px+1] === "f") {
+      tiles[py][px+1] = "S";
+      tiles[py][px] = "+";
+      px += 1;  //changes players current x position after moving
+    }
+  }
+}
+
+
+
 function displayStartScreen() {
   let buttonWidth = 400;
   let buttonHeight = 200;
