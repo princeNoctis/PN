3d794ef913507bcf6bdbe85dac6f40343499826f
Santanu09 <43047324+Santanu09@users.noreply.github.com>
2018-11-28 13:57:08 -0600

    e

--- p5-2/sketch.js
+++ p5-2/sketch.js
@@ -23,7 +23,25 @@ let x, y,px,py;
 let isMovingUp, isMovingDown, isMovingLeft, isMovingRight;
 let gamestate;
 let hoveringButton;
-let song;
+// let song;
+
+class Jitter {
+  constructor(x,y,diameter,speed){
+    this.x = random(width);
+    this.y = random(height);
+    this.diameter = random(10, 30);
+    this.speed = 1;
+  }
+  move() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  }
+
+  displayjit() {
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+}
+
 
 function preload() {
   //load level data
@@ -37,11 +55,13 @@ function preload() {
   dirt = loadImage("assets/empty.png");
   finish = loadImage("assets/finish.png");
   players = loadImage("assets/player.gif");
+  // song = loadSound("assets/song.mp3");
 }
 
 function setup() {
   px = 0;
   py = 0;
+  // song.setVolume(0.5);
   gamestate = 1;
   // keep this a 4:3 ratio, or it will stretch in weird ways
   createCanvas(800, 600);
@@ -74,6 +94,7 @@ function draw() {
   movePlayer();
   if (gamestate === 1) {
     displayStartScreen();
+    // song.loop();
   }
   if (gamestate === 2) {
     display();
@@ -81,8 +102,7 @@ function draw() {
 }
 
 function display() {
-  if (gamestate === 2) {
-    // player();
+  if (gamestate === 1) {
     displayStartScreen();
   }
   if (gamestate === 2){
@@ -97,17 +117,6 @@ function display() {
   // player();
 
 }
-//
-// function player(){
-//   ellipseMode(CENTER);
-//   ellipse(x,y,tilesWide,tilesHigh);
-//   if (y >= 600 || y <= 0) {
-//     y= y * -1.00001;
-//   }
-//   if (x  >= 800 || x <= 0) {
-//     x= x * -1.00001;
-//   }
-// }
 
 function showTile(location, x, y) {
   if (location === "#") {
@@ -158,68 +167,51 @@ function createempty2dArray(cols, rows) {
 function keyPressed() {
   if (keyCode === 87) { //keycode 87 = w
     //condition for checking if the top square is white or green/destination
-    if (tiles[y-1][x] === "+" || tiles[y-1][x] === "f") {
-      tiles[y-1][x] = "S";
-      tiles[y][x] = ".";
-      y -= 1; //changes players current y position after moving
+    if (tiles[py-1][px] === "+" || tiles[py-1][px] === "f") {
+      tiles[py-1][px] = "S";
+      tiles[py][px] = "+";
+      py -= 1; //changes players current y position after moving
     }
   }
   if (keyCode === 65) { //keycode 65 = a
     //condition for checking if the left square is white or green/destination
-    if (tiles[y][x-1] === "+" || tiles[y][x-1] === "f") {
-      tiles[y][x-1] = "S";
-      tiles[y][x] = ".";
-      x -= 1;  //changes players current x position after moving
+    if (tiles[py][px-1] === "+" || tiles[py][px-1] === "f") {
+      tiles[py][px-1] = "S";
+      tiles[py][px] = "+";
+      px -= 1;  //changes players current x position after moving
     }
   }
   if (keyCode === 83) { //keycode 83 = s
     //condition for checking if the bottom square is white or green/destination
-    if (tiles[y+1][x] === "+" || tiles[y+1][x] === "f") {
-      tiles[y+1][x] = "S";
-      tiles[y][x] = ".";
-      y += 1;  //changes players current y position after moving
+    if (tiles[py+1][px] === "+" || tiles[py+1][px] === "f") {
+      tiles[py+1][px] = "S";
+      tiles[py][px] = "+";
+      py += 1;  //changes players current y position after moving
     }
   }
   if (keyCode === 68) { //keycode 68 = d
     //condition for checking if the right square is white or green/destination
-    if (tiles[y][x+1] === "+" || tiles[y][x+1] === "f") {
-      tiles[y][x+1] = "S";
-      tiles[y][x] = ".";
-      x += 1;  //changes players current x position after moving
+    if (tiles[py][px+1] === "+" || tiles[py][px+1] === "f") {
+      tiles[py][px+1] = "S";
+      tiles[py][px] = "+";
+      px += 1;  //changes players current x position after moving
     }
   }
 }
 
 function displayStartScreen() {
+  background(0,0,255);
   let buttonWidth = 300;
   let buttonHeight = 150;
   let leftSide = width / 2 - buttonWidth / 2;
   let topSide = height / 2 - buttonHeight / 2;
   let rightSide = leftSide + buttonWidth;
   let bottomSide = topSide + buttonHeight;
+  let bugs = [];
   fill(255,0,0);
-  background(255,0,0);
-
-  function setup() {
-    // song = loadSoun"3');
-    createCanvas(720, 200);
-    background(255,0,0);
-  }
-
-  function mousePressed() {
-    if ( song.isPlaying() ) {
-      song.stop();
-      background(255,0,0);
-
-
-
-
-    }
-    else {
-      song.play();
-      background(0,255,0);
-    }
-  }
+  // if (gamestate === 2){
+  //   // noLoop(song);
+  // }
   if (mouseX >= leftSide && mouseX <= rightSide && mouseY >= topSide && mouseY <= bottomSide) {
     fill(125);
     if (mouseIsPressed) {
@@ -227,4 +219,11 @@ function displayStartScreen() {
     }
   }
   rect(leftSide, topSide, buttonWidth, buttonHeight);
+  for (let i=0; i<bugs.length; i++) {
+    bugs.move();
+    bugs.displayjit();
+  }
+  for (let i=0; i<50; i++) {
+    bugs.push(new Jitter());
+  }
 }
