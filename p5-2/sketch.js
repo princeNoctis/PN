91686b58767c15d4c431f275a0df3d9f61615376
Santanu09 <43047324+Santanu09@users.noreply.github.com>
2018-11-28 12:39:55 -0600

    Update sketch.js

--- p5-2/sketch.js
+++ p5-2/sketch.js
@@ -201,20 +201,21 @@ function displayStartScreen() {
   background(255,0,0);
 
   function setup() {
-    song = loadSoun"3');
+    // song = loadSoun"3');
     createCanvas(720, 200);
     background(255,0,0);
   }
 
   function mousePressed() {
-    if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    if ( song.isPlaying() ) {
       song.stop();
       background(255,0,0);
 
 
 
 
-    } else {
+    }
+    else {
       song.play();
       background(0,255,0);
     }
@@ -226,39 +227,4 @@ function displayStartScreen() {
     }
   }
   rect(leftSide, topSide, buttonWidth, buttonHeight);
-}
-
-function cur(){
-  let vx = 100,
-    vy = 100,
-    angle1 = 0.0,
-    segLength = 50;
-
-
-  function setup() {
-    createCanvas(710, 400);
-    strokeWeight(20.0);
-    stroke(255, 100);
-  }
-
-  function draw() {
-    background(0);
-
-    let zx = mouseX - x;
-    let zy = mouseY - y;
-    angle1 = atan2(vy, vx);
-    x = mouseX - cos(angle1) * segLength;
-    y = mouseY - sin(angle1) * segLength;
-
-    segment(x, y, angle1);
-    ellipse(mouseX, mouseY, 20, 20);
-    fill();
-  }
-
-  function segment(x, y, a) {
-    push();
-    translate(x, y);
-    rotate(a);
-    pop();
-  }
 }
